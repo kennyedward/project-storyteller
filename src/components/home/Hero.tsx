9009cdbd5a2854d8916e756a688ed76b8771191b
Kenny <kennyedward99@gmail.com>
2025-03-21 08:52:49 +0100

    Update Hero.tsx
    updated subtext margin bottom

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className="py-16 md:py-24 px-8 md:px-16">
       <div className="max-w-6xl mx-auto">
-        <p className="font-manrope text-xl text-[1.25rem] text-neutral-700 dark:text-neutral-300 max-w-3xl mb-8 animate-fade-in" style={{ animationDelay: '0.1s' }}>
+        <p className="font-manrope text-xl text-[1.25rem] text-neutral-700 dark:text-neutral-300 max-w-3xl mb-3 animate-fade-in" style={{ animationDelay: '0.1s' }}>
           Hey, I'm Kehinde Odewole — a digital designer based in Berlin 🇩🇪
         </p>
         <h1 className="font-bricolage text-4xl md:text-6xl font-semibold leading-tight mb-6 animate-fade-in w-4/5" style={{ animationDelay: '0.3s' }}>
